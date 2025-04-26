986d0554138d6e553a1cfe9dd5328db4f5d021e1
Anjan Bellamkonda <bellamkonda.anjan.usa@gmail.com>
2025-04-25 20:58:54 -0400

    model update

--- app.py
+++ app.py
@@ -198,7 +198,7 @@ def ai_maker(prompt, courses):
         api_key=os.getenv("GEMINI_API_KEY"),
     )
 
-    model = "gemini-2.5-flash-preview-04-17"
+    model = "gemini-2.5-pro-preview-03-25"
     max_retries = 5  # Maximum number of retries to find a non-overlapping schedule
     retry_count = 0
 
